4e4445b5e2bcca55cb411e104b83552b9deb0580
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-04-18 18:02:18 +0100

    Better wood bridges

--- make_all.sh
+++ make_all.sh
@@ -3,6 +3,7 @@ bash process_terrain.sh
 bash process_buildings.sh
 bash process_industries.sh
 bash process_infrastructure.sh
+bash process_bridges.sh
 bash process_selectors.sh
 bash process_trees.sh
 bash process_effects.sh
