9bf83209f8bd6b644568259f624381f804bd831d
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-04-19 23:50:58 +0100

    Road depot graphics plus various tweaks

--- graphics/process_stations.sh
+++ graphics/process_stations.sh
@@ -11,6 +11,8 @@ mask_regions.py pygen/monoraildepots monoraildepots_regionmask.png 1
 mask_regions.py pygen/monoraildepots_snow monoraildepots_regionmask.png 1
 mask_regions.py pygen/maglevdepots maglevdepots_regionmask.png 1
 mask_regions.py pygen/maglevdepots_snow maglevdepots_regionmask.png 1
+mask_regions.py pygen/roaddepots roaddepots_regionmask.png 1
+mask_regions.py pygen/roaddepots_snow roaddepots_regionmask.png 1
 custom_dither.py
 cd pygen
 custom_dither.py
