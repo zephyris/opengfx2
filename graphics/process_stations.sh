b8548173cb6ff1efb6df5bbf4567716dd6faa4b8
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-04-24 08:21:21 +0100

    Trial tram depots, monorail stations

--- graphics/process_stations.sh
+++ graphics/process_stations.sh
@@ -4,15 +4,11 @@ cd stations
 
 cd general/64
 building_shapeproc.py 1
-building_shapeproc.py 1 True
 mask_regions.py pygen/raildepots raildepots_regionmask.png 1
-mask_regions.py pygen/raildepots_snow raildepots_regionmask.png 1
 mask_regions.py pygen/monoraildepots monoraildepots_regionmask.png 1
-mask_regions.py pygen/monoraildepots_snow monoraildepots_regionmask.png 1
 mask_regions.py pygen/maglevdepots maglevdepots_regionmask.png 1
-mask_regions.py pygen/maglevdepots_snow maglevdepots_regionmask.png 1
 mask_regions.py pygen/roaddepots roaddepots_regionmask.png 1
-mask_regions.py pygen/roaddepots_snow roaddepots_regionmask.png 1
+mask_regions.py pygen/tramdepots tramdepots_regionmask.png 1
 custom_dither.py
 cd pygen
 custom_dither.py
