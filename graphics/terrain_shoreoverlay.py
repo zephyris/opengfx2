f112d8d5292f11bf19c0ac862882a505eb8f131f
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-04-25 10:55:22 +0100

    Streamline terrain_shoreoverlay.py

--- graphics/terrain_shoreoverlay.py
+++ graphics/terrain_shoreoverlay.py
@@ -6,7 +6,7 @@ from random import randint
 import numpy, blend_modes # For overlay blending
 import glob, os, sys
 
-from tools import openttd_palettise, openttd_palette_image, check_update_needed
+from tools import openttd_palettise, palette_image, openttd_palette_image, check_update_needed, blend_overlay
 
 if os.path.isdir("pygen") == False: os.mkdir("pygen")
 
@@ -14,64 +14,38 @@ verbose = True
 scale = int(sys.argv[1])
 tile_size = scale * 64
 
-palimage=openttd_palette_image()
-
 # Terrain sprites to use
 terrain_list = {
-  "arctic_grass": "arctic_groundtiles_32bpp.png",
-  "arctic_snow": "arctic_groundtiles_snow_32bpp.png",
-  "tropical_grass": "tropical_groundtiles_32bpp.png",
-  "tropical_desert": "tropical_groundtiles_desert_32bpp.png",
-  "temperate_grass": "temperate_groundtiles_32bpp.png",
-  "general_concrete": "general_concretetiles_32bpp.png"
+	"arctic_grass": "arctic_groundtiles_32bpp.png",
+	"arctic_snow": "arctic_groundtiles_snow_32bpp.png",
+	"tropical_grass": "tropical_groundtiles_32bpp.png",
+	"tropical_desert": "tropical_groundtiles_desert_32bpp.png",
+	"temperate_grass": "temperate_groundtiles_32bpp.png",
+	"general_concrete": "general_concretetiles_32bpp.png"
 }
-
-def overlay_simple(image_32bit, image_texture, opacity):
-  image_width, image_height = image_32bit.size
-  # Make black image for merging channels
-  image_black = Image.new("L", (image_width, image_height), 0)
-  image_white = Image.new("L", (image_width, image_height), 255)
-  # Make sharp mask from blue and white pixels in image_32bit (only ones with b == 255)
-  image_mask = Image.new("L", (image_width, image_height), 0)
-  for x in range(image_width):
-    for y in range(image_height):
-      r, g, b, a = image_32bit.getpixel((x, y))
-      if b == 255:
-        image_mask.putpixel((x, y), 255)
-  # Mask overlay with image_mask
-  image_transparent = Image.merge("RGBA", (image_black, image_black, image_black, image_black))
-  image_texture.paste(image_transparent, (0, 0), image_mask)
-  # Overlay texture image
-  r, g, b = image_32bit.convert("RGB").split()
-  image_32bit = Image.merge("RGBA", (r, g, b, image_white))
-  image_bg_arr = numpy.array(image_32bit).astype(float)
-  image_fg_arr = numpy.array(image_texture).astype(float)
-  image_blended_arr_float = blend_modes.overlay(image_bg_arr, image_fg_arr, opacity)
-  image_blended_arr = numpy.uint8(image_blended_arr_float)
-  image_out = Image.fromarray(image_blended_arr)
-  return image_out
 
 print("Running in scale "+str(scale)+" (tile size "+str(tile_size)+")")
 for terrain_key in terrain_list:
-  print(" "+terrain_key)
+  print("  "+terrain_key)
   terrain_image_path = terrain_list[terrain_key]
   shoreline_overlay_path = "shorelines_overlaynormal.png"
   shoreline_shading_path = "shorelines_overlayshading.png"
   output_main_path = os.path.join("pygen", terrain_key+"_shoretiles_palmask.png")
   output_palmask_path = os.path.join("pygen", terrain_key+"_shoretiles_32bpp.png")
   if check_update_needed([terrain_image_path, shoreline_overlay_path, shoreline_shading_path], output_main_path):
-    terrain_image = Image.open(terrain_image_path).convert("RGB")
-    shore_image = Image.open(shoreline_overlay_path)
+    terrain_image = Image.open(terrain_list[terrain_key]).convert("RGB")
+    shore_image = Image.open("shorelines_overlaynormal.png")
     shore_image = openttd_palettise(shore_image)
     width, height = shore_image.size
     target_image = terrain_image.crop((0, 0, width, height))
     # Save shore_image as palmask image
-    shore_image.save(output_main_path)
+    shore_image.save(os.path.join("pygen", terrain_key+"_shoretiles_palmask.png"))
     # Overlay shading, if it exists
     target_image = target_image.convert("RGBA")
-    if os.path.isfile(shoreline_shading_path):
-      shoreline_shading = Image.open(shoreline_shading_path).convert("RGBA")
-      target_image = overlay_simple(target_image, shoreline_shading, 192/255)
+    if os.path.isfile("shorelines_overlayshading.png"):
+      print("shorelines_overlayshading.png")
+      shoreline_shading = Image.open("shorelines_overlayshading.png").convert("RGBA")
+      target_image = blend_overlay(target_image, shoreline_shading, 192/255)
     # Overlay shore_image onto target_image using blue as transparent
     v = [255] * 256
     v[0] = 0
@@ -81,4 +55,4 @@ for terrain_key in terrain_list:
     shore_mask = shore_mask.convert("L")
     target_image.paste(shore_image, (0, 0), shore_mask)
     # Save
-    target_image.save(output_palmask_path)
+    target_image.save(os.path.join("pygen", terrain_key+"_shoretiles_32bpp.png"))
--- graphics/tools.py
+++ graphics/tools.py
@@ -1,40 +1,60 @@
 from PIL import Image
 import numpy
 import blend_modes
+import os
 
+"""
+Standard OpenTTD DOS palette
+"""
 openttd_palette = {
   "r": [0,16,32,48,64,80,100,116,132,148,168,184,200,216,232,252,52,68,88,108,132,156,176,204,48,64,80,96,120,148,176,204,72,88,104,124,152,184,212,244,64,88,112,136,160,188,204,220,236,252,252,252,252,76,96,116,136,156,176,196,68,96,128,156,184,212,232,252,252,252,32,64,84,108,128,148,168,184,196,212,8,16,32,48,64,84,104,128,28,44,60,80,104,128,152,180,16,32,56,76,96,120,152,184,32,56,72,88,104,124,140,160,76,96,116,136,164,184,204,212,224,236,80,100,120,140,160,184,36,48,64,80,100,132,172,212,40,64,88,104,120,140,160,188,0,0,0,0,0,24,56,88,128,188,16,24,40,52,80,116,156,204,172,212,252,252,252,252,252,252,72,92,112,140,168,200,208,232,60,92,128,160,196,224,252,252,252,252,252,252,252,252,204,228,252,252,252,252,8,12,20,28,40,56,72,100,92,108,124,144,224,200,180,132,88,244,245,246,247,248,249,250,251,252,253,254,255,76,108,144,176,210,252,252,252,252,252,252,252,64,255,48,64,80,255,32,36,40,44,48,72,100,216,96,68,255],
   "g": [0,16,32,48,64,80,100,116,132,148,168,184,200,216,232,252,60,76,96,116,140,160,184,208,44,60,76,92,120,148,176,204,44,60,80,104,132,160,188,220,0,4,16,32,56,84,104,132,156,188,208,232,252,40,60,88,116,136,156,180,24,44,68,96,120,156,184,212,248,252,4,20,28,44,56,72,92,108,128,148,52,64,80,96,112,132,148,168,52,68,88,104,124,148,176,204,52,72,96,116,136,164,192,220,24,28,40,52,64,84,108,128,40,52,68,84,96,112,128,148,168,188,28,40,56,76,100,136,40,52,64,80,100,132,172,212,20,44,64,76,88,104,136,168,24,36,52,72,96,120,144,168,196,224,64,80,96,112,140,172,204,240,52,52,52,100,144,184,216,244,20,44,68,100,136,176,184,208,0,0,0,0,0,0,0,80,108,136,164,192,220,252,136,144,156,176,196,216,24,36,52,68,92,120,152,172,156,176,200,224,244,236,220,188,152,0,0,0,0,0,0,0,0,0,0,0,0,24,44,72,108,146,60,84,104,124,148,172,196,0,0,48,64,80,255,68,72,76,80,84,100,132,244,128,96,255],
   "b": [255,16,32,48,64,80,100,116,132,148,168,184,200,216,232,252,72,92,112,132,152,172,196,220,4,12,20,28,64,100,132,168,4,20,44,72,92,120,148,176,4,16,32,52,76,108,124,144,164,192,0,60,128,0,8,28,56,80,108,136,0,4,8,16,24,32,16,0,128,192,0,8,16,28,40,56,76,88,108,128,0,0,4,4,12,20,28,44,24,32,48,60,76,92,108,124,24,44,72,88,108,136,168,200,0,0,4,12,24,44,64,88,16,24,40,56,64,80,96,112,128,148,4,20,40,64,96,136,68,84,100,116,136,164,192,224,112,144,172,196,224,252,252,252,108,132,160,184,212,220,232,240,252,252,96,108,120,132,160,192,220,252,52,52,52,88,124,160,200,236,112,140,168,196,224,248,255,252,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,4,0,48,100,152,88,104,124,140,164,188,216,224,52,64,76,92,252,248,236,216,172,244,245,246,247,248,249,250,251,252,253,254,255,8,24,52,84,126,0,0,0,0,0,0,0,0,0,0,0,0,0,112,116,120,124,128,144,168,252,164,140,255]
 }
 
+"""
+Palette indices of special colours
+"""
 openttd_palette_animated = [227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,245,246,247,248,249,250,251,252,253,254,254]
 openttd_palette_generalmask = [0,255]
 
-# Make a minimal image containing all OpenTTD palette values
+def palette_image(r, g, b):
+  """
+  Make a minimal image containing palette values
+  :return: A 256 by 1 image in `P` mode with the specified r, g, b palette
+  """
+  palette = []
+  for i in range(len(r)):
+    palette.append(r[i])
+    palette.append(g[i])
+    palette.append(b[i])
+  palimage = Image.new('P', (256, 1))
+  for x in range(256):
+    palimage.putpixel((x, 0), x)
+  palimage.putpalette(palette)
+  return palimage
+
 def openttd_palette_image():
+  """
+  Make a minimal image containing all OpenTTD palette values
+  :return: A 256 by 1 image in `P` mode with the OpenTTD DOS palette
+  """
   palette_r = openttd_palette["r"]
   palette_g = openttd_palette["g"]
   palette_b = openttd_palette["b"]
-  def palette_image(r, g, b):
-    palette = []
-    for i in range(len(r)):
-      palette.append(r[i])
-      palette.append(g[i])
-      palette.append(b[i])
-    palimage = Image.new('P', (256, 1))
-    for x in range(256):
-      palimage.putpixel((x, 0), x)
-    palimage.putpalette(palette)
-    return palimage
   palette = palette_image(palette_r, palette_g, palette_b)
   return palette
 
-# Convert an image to an 8-bit palette image with the OpenTTD palette
-# Conventional conversion would be source.quantize(palette=[openttdpalette], dither=False)
-# DON'T EVER USE THAT! There is a bug which leads to clipping of r, g or b values to 252 prior to palette index assignment
-# ... plus, I think mine looks nicer
 def openttd_palettise(source):
+  """
+  Convert an image to an 8-bit palette image with the OpenTTD palette. Does not do dithering.
+  :param source: Input image, will be treated as RGB.
+  :return: Image converted to the OpenTTD palette
+  """
+  # Conventional conversion would be source.quantize(palette=[openttdpalette], dither=False)
+  # DON'T EVER USE THAT! There is a bug which leads to clipping of r, g or b values to 252 prior to palette index assignment
+  # Plus, I think mine looks nicer
+
   r = openttd_palette["r"]
   g = openttd_palette["g"]
   b = openttd_palette["b"]
@@ -66,8 +86,6 @@ def openttd_palettise(source):
             index = i
         target.putpixel((x, y), index)
   return target
-
-import os
 
 def check_update_needed(input_file_list, output_file):
   """
@@ -189,3 +207,36 @@ def bluewhite_to_transp(source):
       if b == 255:
         source.putpixel((x, y), (r, g, b, 0))
   return source
+
+def blend_overlay(image_32bit, image_texture, opacity):
+  """
+  Applies an image texture, overlayed with a custom opacity, on an RGB image. Uses blend_modes.overlay
+  :param image_32bit: Base image, treated as RGB
+  :param image_texture: Image to overlay, 
+  :param opacity: Overlay opacity
+  :return: 
+  """
+  # Used to be named `overlay_simple`
+  image_width, image_height = image_32bit.size
+  # Make black image for merging channels
+  image_black = Image.new("L", (image_width, image_height), 0)
+  image_white = Image.new("L", (image_width, image_height), 255)
+  # Make sharp mask from blue and white pixels in image_32bit (only ones with b == 255)
+  image_mask = Image.new("L", (image_width, image_height), 0)
+  for x in range(image_width):
+    for y in range(image_height):
+      r, g, b, a = image_32bit.getpixel((x, y))
+      if b == 255:
+        image_mask.putpixel((x, y), 255)
+  # Mask overlay with image_mask
+  image_transparent = Image.merge("RGBA", (image_black, image_black, image_black, image_black))
+  image_texture.paste(image_transparent, (0, 0), image_mask)
+  # Overlay texture image
+  r, g, b = image_32bit.convert("RGB").split()
+  image_32bit = Image.merge("RGBA", (r, g, b, image_white))
+  image_bg_arr = numpy.array(image_32bit).astype(float)
+  image_fg_arr = numpy.array(image_texture).astype(float)
+  image_blended_arr_float = blend_modes.overlay(image_bg_arr, image_fg_arr, opacity)
+  image_blended_arr = numpy.uint8(image_blended_arr_float)
+  image_out = Image.fromarray(image_blended_arr)
+  return image_out
