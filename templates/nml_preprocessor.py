938806c0e124c642c8d8bfa8999535774886e2ba
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-06-26 22:29:57 +0100

    Codechange: Add nml pre-processer option to exclude extra zoom or 32bpp sprites

--- templates/nml_preprocessor.py
+++ templates/nml_preprocessor.py
@@ -1,10 +1,15 @@
 """
 Loads a pnml file and:
 Populates with information from #import lines.
+Removes comments to configure extra zoom and 32bpp alternative sprites.
 
 Import lines in the pnml must be formatted:
 #import "path/to/nml/file.nml"
 Path must be quoted, no additional whitespace is tolerated.
+
+Extra zoom and 32bpp alternative sprites are controlled by removing comment strings exactly matching:
+"#ez" for extra zoom sprites
+"#32" for 32bpp sprites
 
 Argument:
 pnml name (without extension)
@@ -13,18 +18,34 @@ Output:
 nml file with the same name as the pnml file
 """
 
-import_comment = "#include"
+comment_import = "#include"
+
+comment_alternate = ["#ez ", "#32 "]
+settings_alternate = [True, False]
 
 import sys
+
+def handle_alternates(line, options = []):
+  for c in range(len(comment_alternate)):
+    comment = comment_alternate[c]
+    if comment in line:
+      if settings_alternate[c]:
+        line = line.replace(comment, "")
+      else:
+        line = line.replace(comment, comment.replace("#", "//"))
+  return line
 
 with open(sys.argv[1]+".nml", "w") as nml:
   with open(sys.argv[1]+".pnml", "r") as pnml:
     lines = pnml.read().splitlines()
     for line in lines:
-      if line.startswith(import_comment):
-        path = line[len(import_comment)+2:-1]
+      if line.startswith(comment_import):
+        path = line[len(comment_import)+2:-1]
         with open(path, "r") as include:
-          include_text = include.read()
-          nml.write(include_text)
+          include_lines = include.read().splitlines()
+          for include_line in include_lines:
+            include_line = handle_alternates(include_line)
+            nml.write(include_line + "\n")
       else:
+        include_line = handle_alternates(include_line)
         nml.write(line + "\n")
