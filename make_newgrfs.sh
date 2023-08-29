93feb7396f198f455100fe647726122ed089bade
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-08-29 21:23:55 +0100

    Feature: Standalone landscape newgrf

--- make_newgrfs.sh
+++ make_newgrfs.sh
@@ -4,9 +4,16 @@
 # eg. /my/install/of/openttd/newgrf/
 
 cd newgrf
+
+# Settings
 python3 ../templates/nml_preprocessor.py ogfx2_settings 8
 mv ogfx2_settings_8.nml ogfx2_settings.nml
 nmlc ogfx2_settings.nml --quiet -c -l lang/settings
+
+# Landscape
+python3 ../templates/nml_preprocessor.py ogfx2_landscape 32ez
+mv ogfx2_settings_32ez.nml ogfx2_settings.nml
+nmlc ogfx2_landscape.nml --quiet -c -l lang/landscape
 
 if [ ! -z "$1" ]; then
   cp *.grf "$1"
