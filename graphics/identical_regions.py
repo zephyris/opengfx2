146878ab80dcc677b25354189ae19e7500fe32e5
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-10-04 23:13:34 +0100

    Fix: Make major shared faces of foundations share appearance
    Fixes #27

--- graphics/identical_regions.py
+++ graphics/identical_regions.py
@@ -17,29 +17,30 @@ mask_path = sys.argv[2]
 for suffix in suffices:
   source_path = base_path + suffix
   out_path = base_path + "_idmap" + suffix
-  if check_update_needed([source_path, mask_path], out_path):
-    source_image = Image.open(source_path)
-    # 8-bit indexed or grayscale image, each value indicates a rectangular region
-    # Must be sequentially numbered, indices 1..255 represent regions to make identical, 0 indicates background
-    region_mask = openttd_palettise(Image.open(mask_path))
-    
-    # Work in numpy/skimage
-    region_mask = numpy.array(region_mask)
-    region_number = numpy.max(region_mask) 
-    print("Number of regions in identical region mask:", region_number)
-    for i in range(region_number):
-      # make mask equal to current index value
-      current_mask = numpy.where(region_mask == i + 1, 255, 0)
-      current_labels = skimage.measure.label(current_mask)
-      mask_props = skimage.measure.regionprops_table(current_labels, properties=["bbox"])
-      # master region
-      refx1, refy1, refx2, refy2 = mask_props["bbox-0"][0], mask_props["bbox-1"][0], mask_props["bbox-2"][0], mask_props["bbox-3"][0]
-      print("Master region:", refy1, refx1, refy2, refx2) # x/y swapped in numpy representation
-      count = len(mask_props["bbox-0"])
-      mask = Image.fromarray(numpy.uint8(current_mask)).crop((refy1, refx1, refy2, refx2))
-      # take first entry as master copy, and do masked copy to all other positions
-      for i in range(1, count):
-        print("  Pasting to:", mask_props["bbox-1"][i], mask_props["bbox-0"][i], mask_props["bbox-3"][i], mask_props["bbox-2"][i])
-        source_image.paste(source_image.crop((refy1, refx1, refy2, refx2)), (mask_props["bbox-1"][i], mask_props["bbox-0"][i]), mask)
-    # save modified source_image as output
-    source_image.save(out_path)
+  if os.file.isfile(source_path):
+    if check_update_needed([source_path, mask_path], out_path):
+      source_image = Image.open(source_path)
+      # 8-bit indexed or grayscale image, each value indicates a rectangular region
+      # Must be sequentially numbered, indices 1..255 represent regions to make identical, 0 indicates background
+      region_mask = openttd_palettise(Image.open(mask_path))
+      
+      # Work in numpy/skimage
+      region_mask = numpy.array(region_mask)
+      region_number = numpy.max(region_mask) 
+      print("Number of regions in identical region mask:", region_number)
+      for i in range(region_number):
+        # make mask equal to current index value
+        current_mask = numpy.where(region_mask == i + 1, 255, 0)
+        current_labels = skimage.measure.label(current_mask)
+        mask_props = skimage.measure.regionprops_table(current_labels, properties=["bbox"])
+        # master region
+        refx1, refy1, refx2, refy2 = mask_props["bbox-0"][0], mask_props["bbox-1"][0], mask_props["bbox-2"][0], mask_props["bbox-3"][0]
+        print("Master region:", refy1, refx1, refy2, refx2) # x/y swapped in numpy representation
+        count = len(mask_props["bbox-0"])
+        mask = Image.fromarray(numpy.uint8(current_mask)).crop((refy1, refx1, refy2, refx2))
+        # take first entry as master copy, and do masked copy to all other positions
+        for i in range(1, count):
+          print("  Pasting to:", mask_props["bbox-1"][i], mask_props["bbox-0"][i], mask_props["bbox-3"][i], mask_props["bbox-2"][i])
+          source_image.paste(source_image.crop((refy1, refx1, refy2, refx2)), (mask_props["bbox-1"][i], mask_props["bbox-0"][i]), mask)
+      # save modified source_image as output
+      source_image.save(out_path)
