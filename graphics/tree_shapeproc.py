cfe07f4cd8b62fdb9c31e4f27f56b80a4aa75b25
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-11-07 17:52:50 +0000

    Graphics: New 1x and 4x tropical palm trees and cacti
    Not coded yet

--- graphics/tree_shapeproc.py
+++ graphics/tree_shapeproc.py
@@ -70,103 +70,143 @@ def add_value_noise(image_32bit, image_shape, indices, power):
   image_32bit = image_hsv.convert("RGB")
   return image_32bit
 
-suffix = "_leaf_shape.png";
-# Input image must be forrectly formatted:
-# Sprites must be organised in with a 1px white (index index_frame) border
-# Sprites must be organised in columns, each column defines a tree
-# Sprites must be the exact sprite_width/height
-# Source indices for remapping:
-index_leaf_remaps = [80, 81, 82, 83, 84, 85, 86, 87]
-index_trunk_remaps = [104, 105, 106, 107, 108, 109, 110, 111]
-index_remaps_snow = [-1, -1, -1, 213, 212, 211, 210, 210]
-#index_remaps_snow = index_leaf_remaps[4:] + index_trunk_remaps[4:]
-# Tree sprite sizes
-w = 45
-h = 80
-# Growth/death parameters
-cy = 6 # y position of centre of base of trunk relative to bottom of sprite
-resize = [0.45, 0.65, 0.85, 1, 1, 1, 1] # scale factor
-density = [1, 1, 1, 1, 0.8, 0.5, 0] # leaf density, random pixel removal
-for input_file in glob.glob("*"+suffix):
-  outfile = os.path.join("pygen", input_file[:-len(suffix)]+"_"+namesuffix+"32bpp.png")
-  if check_update_needed([input_file], outfile):
-    with Image.open(input_file) as image:
-      # Open shape image
-      width, height = image.size
-      columns = int(math.floor(width / w) / scale)
-      print("%s in scale %d mode" % (input_file, scale))
-      # Convert shape image to OpenTTD 8 bit
-      image_shape = openttd_palettise(image)
-      # Apply colouring specified by pixel above each sprite
-      image_colorised = Image.new("P", (image_shape.size))
-      image_colorised.paste(image_shape, (0,0))
-      image_colorised.putpalette(image_shape.getpalette())
-      for column in range(columns):
-        x = column * (w + 1) + 1
-        for row in range(2):
-          y = row * (h + 1) + 1
-          print("  bounds "+", ".join([str(x) for x in [x, y, w, h]]))
-          index_target = []
-          if row == 0:
-            print("  leaf")
-            index_remaps = index_leaf_remaps
-          else:
-            print("  trunk")
-            index_remaps = index_trunk_remaps
-          for i in range(len(index_remaps)):
-            index_target.append(image_shape.getpixel(((x + i) * scale, (y - 1) * scale)))
-          if verbose == True:
-            print("  Source indices: "+",".join(map(str, index_remaps)))
-            print("  Target indices: "+",".join(map(str, index_target)))
-          # Apply new colours
-          current_r = palette_r.copy()
-          current_g = palette_g.copy()
-          current_b = palette_b.copy()
-          for i in range(len(index_remaps)):
-            current_r[index_remaps[i]] = palette_r[index_target[i]]
-            current_g[index_remaps[i]] = palette_g[index_target[i]]
-            current_b[index_remaps[i]] = palette_b[index_target[i]]
-          # Override with snow colors, if in snow mode 
-          if snow == True:
-            for i in range(len(index_remaps_snow)):
-              if index_remaps_snow[i] != -1:
-                current_r[index_remaps[i]] = palette_r[index_remaps_snow[i]]
-                current_g[index_remaps[i]] = palette_g[index_remaps_snow[i]]
-                current_b[index_remaps[i]] = palette_b[index_remaps_snow[i]]
-          current_palimg = palette_image(current_r, current_g, current_b)
-          current_spriteset = image_colorised.crop((x * scale, (y - 1) * scale, (x + w) * scale, (y + h) * scale))
-          current_spriteset.putpalette(current_palimg.getpalette())
-          current_spriteset = openttd_palettise(current_spriteset)
-          image_colorised.paste(current_spriteset, (x * scale, (y - 1) * scale))
-      # Generate tree growth and death sprites
-      current_palimg = palette_image(current_r, current_g, current_b)
-      image_out = Image.new("RGBA", (((w + 1) * 7 + 1) * scale, ((h + 1) * columns + 1) * scale), (0, 0, 255))
-      draw = ImageDraw.Draw(image_out)
-      for column in range(columns):
-        x = column * (w + 1) + 1
-        for outcolumn in range(7):
-          for row in [0, 1, 0]:
+suffices = {
+  "_leaf_shape.png": {
+    "name": "",
+    "resize": [0.45, 0.65, 0.85, 1, 1, 1, 1], # scale factor
+    "density": [1, 1, 1, 1, 0.8, 0.5, 0], # leaf density, random pixel removal
+    "browning": [1, 1, 1, 1, 1, 1, 1], # leaf browning, random pixel brownout
+    "trunkshift": [0, 0, 0, 0, 0, 0, 0], # trunk index darkening shift
+  },
+  "_nonleaf_shape.png": {
+    "name": "nonleaf_",
+    "resize": [0.45, 0.65, 0.85, 1, 0.92, 0.82, 0.67], # scale factor
+    "density": [1, 1, 1, 1, 1, 1, 1], # leaf density, random pixel removal
+    "browning": [1, 1, 1, 1, 0.75, 0.5, 0], # leaf browning, random pixel brownout
+    "trunkshift": [0, 0, 0, 0, 0, 1, 2], # trunk index darkening shift
+  }
+}
+for suffix in suffices:
+  # Input image must be forrectly formatted:
+  # Sprites must be organised in with a 1px white (index index_frame) border
+  # Sprites must be organised in columns, each column defines a tree
+  # Sprites must be the exact sprite_width/height
+  # Source indices for remapping:
+  index_leaf_remaps = [80, 81, 82, 83, 84, 85, 86, 87]
+  index_trunk_remaps = [104, 105, 106, 107, 108, 109, 110, 111]
+  index_remaps_snow = [-1, -1, -1, 213, 212, 211, 210, 210]
+  index_browning = [104, 105, 106, 107, 108, 109, 110, 111]
+  #index_remaps_snow = index_leaf_remaps[4:] + index_trunk_remaps[4:]
+  # Tree sprite sizes
+  w = 45
+  h = 80
+  # Growth/death parameters
+  cy = 6 # y position of centre of base of trunk relative to bottom of sprite
+  resize = suffices[suffix]["resize"] # scale factor
+  density = suffices[suffix]["density"] # leaf density, random pixel removal
+  browning = suffices[suffix]["browning"] # leaf browning, random pixel brownout
+  trunkshift = suffices[suffix]["trunkshift"] # trunk index darkening shift
+  for input_file in glob.glob("*"+suffix):
+    outfile = os.path.join("pygen", input_file[:-len(suffix)]+"_"+suffices[suffix]["name"]+namesuffix+"32bpp.png")
+    if check_update_needed([input_file], outfile):
+      with Image.open(input_file) as image:
+        # Open shape image
+        width, height = image.size
+        columns = int(math.floor(width / w) / scale)
+        print("%s in scale %d mode" % (input_file, scale))
+        # Convert shape image to OpenTTD 8 bit
+        image_shape = openttd_palettise(image)
+        # Apply colouring specified by pixel above each sprite
+        image_colorised = Image.new("P", (image_shape.size))
+        image_colorised.paste(image_shape, (0,0))
+        image_colorised.putpalette(image_shape.getpalette())
+        for column in range(columns):
+          x = column * (w + 1) + 1
+          for row in range(2):
             y = row * (h + 1) + 1
-            current_spriteset = image_colorised.crop((x * scale, y * scale, (x + w) * scale, (y + h) * scale))
-            ow = int(w * resize[outcolumn] * scale)
-            oh = int(h * resize[outcolumn] * scale)
-            xoffs = int(w / 2 - w * resize[outcolumn] / 2)
-            yoffs = int(h - h * resize[outcolumn] + cy * resize[outcolumn] - cy)
-            current_spriteset=current_spriteset.resize((ow, oh), resample=Image.NEAREST)
+            print("  bounds "+", ".join([str(x) for x in [x, y, w, h]]))
+            index_target = []
             if row == 0:
-              seed(0)
-              if density[outcolumn] < 1: # if pixels should be erased
-                for tx in range(scale, ow - scale): # for all pixels, with padding of scale
-                  for ty in range(scale, oh - scale):
-                    if randint(0, 100) > density[outcolumn] * 100: # remove pixels to target density
-                      if randint(1, scale ** 2) == 1: # removing in clumps, so 1 per scale^2
-                        for oa in range(-int(scale / 2) - 1, int(scale / 2) + 1): # for offsets in clump size
-                          for ob in range(-int(scale / 2) - 1, int(scale / 2) + 1):
-                            if (oa ** 2 + ob ** 2) ** 0.5 < (scale / 2) * 1.3 or randint(0, 1) == 0: # circle + random pixels, not square
-                              current_spriteset.putpixel((tx + oa, ty + ob), 0)
-            if row !=0 or density[outcolumn] > 0: # only draw if the trunk (row 1) or density is non-zero
-              image_out = blue_to(current_spriteset, 0, 0, ow, oh, image_out, ((w + 1) * outcolumn + 1 + xoffs), ((h + 1) * column + 1 + yoffs), scale)
-            if row == 1: # draw rectangle after drawing
-              draw.rectangle((((w + 1) * outcolumn) * scale, ((h + 1) * column) * scale, ((w + 1) * outcolumn) * scale + (w + 2) * scale - 1, ((h + 1) * column) * scale + (h + 2) * scale - 1), fill=None, outline=(255, 255, 255), width=scale)
-      # Save a copy of the unshaded image, as palmask
-      image_out.save(outfile, "PNG")
+              print("  leaf")
+              index_remaps = index_leaf_remaps
+            else:
+              print("  trunk")
+              index_remaps = index_trunk_remaps
+            for i in range(len(index_remaps)):
+              index_target.append(image_shape.getpixel(((x + i) * scale, (y - 1) * scale)))
+            if row == 0:
+              index_target_leaf = index_target.copy()
+            if row == 1:
+              index_target_trunk = index_target.copy()
+            if verbose == True:
+              print("  Source indices: "+",".join(map(str, index_remaps)))
+              print("  Target indices: "+",".join(map(str, index_target)))
+            # Apply new colours
+            current_r = palette_r.copy()
+            current_g = palette_g.copy()
+            current_b = palette_b.copy()
+            for i in range(len(index_remaps)):
+              current_r[index_remaps[i]] = palette_r[index_target[i]]
+              current_g[index_remaps[i]] = palette_g[index_target[i]]
+              current_b[index_remaps[i]] = palette_b[index_target[i]]
+            # Override with snow colors, if in snow mode 
+            if snow == True:
+              for i in range(len(index_remaps_snow)):
+                if index_remaps_snow[i] != -1:
+                  current_r[index_remaps[i]] = palette_r[index_remaps_snow[i]]
+                  current_g[index_remaps[i]] = palette_g[index_remaps_snow[i]]
+                  current_b[index_remaps[i]] = palette_b[index_remaps_snow[i]]
+            current_palimg = palette_image(current_r, current_g, current_b)
+            current_spriteset = image_colorised.crop((x * scale, (y - 1) * scale, (x + w) * scale, (y + h) * scale))
+            current_spriteset.putpalette(current_palimg.getpalette())
+            current_spriteset = openttd_palettise(current_spriteset)
+            image_colorised.paste(current_spriteset, (x * scale, (y - 1) * scale))
+        # Generate tree growth and death sprites
+        current_palimg = palette_image(current_r, current_g, current_b)
+        image_out = Image.new("RGBA", (((w + 1) * 7 + 1) * scale, ((h + 1) * columns + 1) * scale), (0, 0, 255))
+        draw = ImageDraw.Draw(image_out)
+        for column in range(columns):
+          x = column * (w + 1) + 1
+          for outcolumn in range(7):
+            for row in [0, 1, 0]:
+              y = row * (h + 1) + 1
+              current_spriteset = image_colorised.crop((x * scale, y * scale, (x + w) * scale, (y + h) * scale))
+              ow = int(w * resize[outcolumn] * scale)
+              oh = int(h * resize[outcolumn] * scale)
+              xoffs = int(w / 2 - w * resize[outcolumn] / 2)
+              yoffs = int(h - h * resize[outcolumn] + cy * resize[outcolumn] - cy)
+              current_spriteset=current_spriteset.resize((ow, oh), resample=Image.NEAREST)
+              if row == 0: # erase some leaf pixels for patchy apperance
+                seed(0)
+                if density[outcolumn] < 1: # if pixels should be erased
+                  for tx in range(scale, ow - scale): # for all pixels, with padding of scale
+                    for ty in range(scale, oh - scale):
+                      if randint(0, 100) > density[outcolumn] * 100: # remove pixels to target density
+                        if randint(1, scale ** 2) == 1: # removing in clumps, so 1 per scale^2
+                          for oa in range(-int(scale / 2) - 1, int(scale / 2) + 1): # for offsets in clump size
+                            for ob in range(-int(scale / 2) - 1, int(scale / 2) + 1):
+                              if (oa ** 2 + ob ** 2) ** 0.5 < (scale / 2) * 1.3 or randint(0, 1) == 0: # circle + random pixels, not square
+                                current_spriteset.putpixel((tx + oa, ty + ob), 0)
+              if row == 0: # brown-out some leaf pixels for dead appearance
+                if browning[outcolumn] < 1: # if pixels should be browned
+                  for tx in range(scale, ow): # for all pixels
+                    for ty in range(scale, oh):
+                      v = current_spriteset.getpixel((tx, ty))
+                      if randint(0, 100) > browning[outcolumn] * 100 and v != 0: # brown pixels to target density
+                        for i in range(len(index_target_leaf)):
+                          if v == index_target_leaf[i]:
+                            current_spriteset.putpixel((tx, ty), index_browning[i])
+              if row == 1: # index-shift darker some trunk pixels
+                if trunkshift[outcolumn] != 0: # if pixel indices should be shifted
+                  for tx in range(scale, ow): # for all pixels
+                    for ty in range(scale, oh):
+                      v = current_spriteset.getpixel((tx, ty))
+                      if v in index_target_trunk: # trunk pixels to value shift
+                        if index_target_trunk.index(v) > trunkshift[outcolumn]:
+                          current_spriteset.putpixel((tx, ty), index_target_trunk[index_target_trunk.index(v) - trunkshift[outcolumn]])
+              if row !=0 or density[outcolumn] > 0: # only draw if the trunk (row 1) or density is non-zero
+                image_out = blue_to(current_spriteset, 0, 0, ow, oh, image_out, ((w + 1) * outcolumn + 1 + xoffs), ((h + 1) * column + 1 + yoffs), scale)
+              if row == 1: # draw rectangle after drawing
+                draw.rectangle((((w + 1) * outcolumn) * scale, ((h + 1) * column) * scale, ((w + 1) * outcolumn) * scale + (w + 2) * scale - 1, ((h + 1) * column) * scale + (h + 2) * scale - 1), fill=None, outline=(255, 255, 255), width=scale)
+        # Save a copy of the unshaded image, as palmask
+        image_out.save(outfile, "PNG")
