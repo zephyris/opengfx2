f039f5429505f8f468ca5ada7c7ab73c2f0b5208
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2023-11-16 22:46:49 +0000

    Codechange: Remove legacy tree graphics

--- graphics/process_trees.sh
+++ graphics/process_trees.sh
@@ -3,21 +3,14 @@ export PATH=$PATH:$(pwd)
 cd trees
 
 cd 64
-custom_dither.py
-cd ..
-
-cd 64_alt
 tree_shapeproc.py 1
 tree_shapeproc.py 1 True
+custom_dither.py
 cd pygen
 custom_dither.py
 cd ../..
 
 cd 256
-custom_dither.py
-cd ..
-
-cd 256_alt
 tree_shapeproc.py 4
 tree_shapeproc.py 4 True
 cd pygen
