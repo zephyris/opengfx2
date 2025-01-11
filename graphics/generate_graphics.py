eb9fcb995061dd93359417a9be2260f3c6c6f31a
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2025-01-11 20:30:52 +0000

    Codechange: Refactor bridge infrastructure overlay

--- graphics/generate_graphics.py
+++ graphics/generate_graphics.py
@@ -6,3 +6,14 @@ base_path = os.path.dirname(__file__)
 # effects
 custom_dither_directory(os.path.join(base_path, "effects", "64"))
 custom_dither_directory(os.path.join(base_path, "effects", "256"))
+
+# infrastructure
+# bridges
+from bridges.bridge_infrastructureoverlay import infrastructure_bridge_infrastructureoverlay
+modes = ["bridges", "railramps", "roadramps", "bridges_toyland", "railramps_toyland", "roadramps_toyland"]
+scales = [1, 4]
+for scale in scales:
+    for mode in modes:
+        infrastructure_bridge_infrastructureoverlay(scale, mode, os.path.join(base_path, "bridges", str(scale * 64)))
+    custom_dither_directory(os.path.join(base_path, "bridges", str(scale * 64)))
+    custom_dither_directory(os.path.join(base_path, "bridges", str(scale * 64), "pygen"))
