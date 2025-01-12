0ff09c83d97bd47b816e7a7face1b0f944efa3f8
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2025-01-12 20:55:09 +0000

    Fix: Run correct python script for generating graphics

--- make_all.sh
+++ make_all.sh
@@ -4,7 +4,7 @@
 # eg. /my/install/of/openttd/
 
 bash make_graphics.sh
-python3 graphics/make_graphics.py
+python3 graphics/generate_graphics.py
 
 if [ ! -z "$1" ]; then
   bash make_newgrfs.sh "$1/newgrf/"
