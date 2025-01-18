744d3ef9207a2e44176875052adc77f149aaf0d2
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2025-01-18 13:43:42 +0000

    Codechange: Clean up unneeded bash scripts
    Remove last legacy bash scripts for building. Fixes #15

--- make_graphics.sh
+++
@@ -1,8 +0,0 @@
-# Make all graphics filed
-# Processes image files from sources
-
-if [ ! -d graphics/fonts/openttd-ttf ]; then
-    cd graphics/fonts && git clone https://github.com/zephyris/openttd-ttf
-fi
-cd graphics/fonts && git pull
-python3 graphics/generate_graphics.py
