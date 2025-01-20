2e6e71f9e9177caf7b011faafcae386a3da49eca
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2025-01-20 12:24:46 +0000

    Fix: Region masking was broken in several ways
    Incorrect blue background for masked sprites was a symptom of handling masking for an entire column of sprites, but still iterating over rows. Required much fixing(!) Fixes #105

--- graphics/mask_regions.py
+++ graphics/mask_regions.py
@@ -6,6 +6,7 @@ import os, sys
 from tools import openttd_palettise, check_update_needed
 
 def mask_regions(base_name, region_mask_path, scale, verbose=True):
+  print("Masking regions in", base_name)
   base_path = os.path.dirname(base_name)
   if os.path.isdir(os.path.join(base_path, "pygen")) == False: os.mkdir(os.path.join(base_path, "pygen"))
 
@@ -21,60 +22,70 @@ def mask_regions(base_name, region_mask_path, scale, verbose=True):
     source_path = base_name + source_suffices[image_index]
     out_path = base_name+"_regions"+source_suffices[image_index]
     if check_update_needed([__file__, source_path, region_mask_path], out_path):
+      print("Generating", os.path.basename(out_path))
       source_image = Image.open(source_path).convert("RGB")
       # 8-bit indexed or grayscale image, each value indicates a region
       # Must be sequentially numbered, indices 1..255 represent subtiles, 0 indicates background
       region_mask = openttd_palettise(Image.open(region_mask_path))
+      # infer tile width from source image, 1 column with scale-wide white border on left and right
       tile_size = source_image.width - scale * 2
+      # infer tile height from non-white pixels in second pixel column
+      tile_height = 0
+      while tile_height < source_image.height:
+        if source_image.getpixel((scale, tile_height + scale)) == (255, 255, 255):
+          break
+        tile_height += 1
+      # round height to multiple of scale
+      tile_height = (tile_height // scale) * scale
+      print("  ", "Tile size", tile_size, ",", tile_height)
 
-      # Process source_image in rows of region_mask height (plus scale px border)
+      # Pre-process masks to set index 255 pixels to index 0
       region_mask_width, region_mask_height = region_mask.size
       for x in range(region_mask_width):
         for y in range(region_mask_height):
           if region_mask.getpixel((x, y)) == 255:
             region_mask.putpixel((x, y), 0)
-      region_mask_height = region_mask_height - scale # Remove lower border from height
 
-      # Find number of subregions from brightest region_mask 
+      # Find number of subregions from brightest region_mask, this will define number of colums in the output spritesheet
       tmp, region_number = region_mask.getextrema()
-      print("Number of regions in region mask:", region_number)
+      print("  ", "Number of regions in region mask:", region_number)
 
       # Setup output image
       source_image_width, source_image_height = source_image.size
-      output_rows = (source_image_height - 1) // (region_mask_height - 1)
-      output_image = Image.new("RGB", ((region_number * (tile_size + scale) + scale), region_mask_height * output_rows + scale), (255, 255, 255))
+      output_rows = (source_image_height - scale) // tile_height
+      print("  ", "Output columns and rows", region_number, ",", output_rows)
+      output_image = Image.new("RGB", ((region_number * (tile_size + scale) + scale), (tile_height + scale) * output_rows + scale), (255, 255, 255))
 
-      # Loop through rows
-      for row in range(output_rows):
-        # Loop through tiles
-        print(" Row "+str(row))
-        for tile in range(region_number):
-          print("  Region index "+str(tile))
-          # Make a mask of the current index
-          values = [0] * 256
-          values[tile + 1] = 255
-          palette = []
-          for i in range(len(values)):
-            palette.append(values[i])
-            palette.append(values[i])
-            palette.append(values[i])
-          current_mask = region_mask.copy()
-          current_mask.putpalette(palette)
-          current_mask = current_mask.convert("L")
-          # define bounds
-          bounds = scale, scale, tile_size + scale, region_mask_height + scale
+      for tile in range(region_number):
+        print("  Region index "+str(tile))
+        # Make a mask of the current index
+        values = [0] * 256
+        values[tile + 1] = 255
+        palette = []
+        for i in range(len(values)):
+          palette.append(values[i])
+          palette.append(values[i])
+          palette.append(values[i])
+        current_mask = region_mask.copy()
+        current_mask.putpalette(palette)
+        current_mask = current_mask.convert("L")
+        # Fill background of sprite blue
+        drawing = ImageDraw.Draw(output_image)
+        for row in range(output_rows):
+          blue_x = scale + tile * (tile_size + scale)
+          blue_y = scale + row * (tile_height + scale)
+          blue_w = tile_size
+          blue_h = tile_height + scale - 1
+          drawing.rectangle((blue_x, blue_y, blue_x + blue_w - 1, blue_y + blue_h - 1), fill="#0000ff", outline=None)
+          # Paste sprite into output using mask
+          bounds = scale, scale + row * (tile_height + scale), scale + tile_size, scale + row * (tile_height + scale) + tile_height + scale
           print("   ", "Mask position  ", tile, bounds)
-          # Fill background of sprite blue
-          sprite_x = scale + tile * (tile_size + scale)
-          sprite_y = scale + row * (region_mask_height + scale)
-          drawing = ImageDraw.Draw(output_image)
-          drawing.rectangle((sprite_x, sprite_y, sprite_x + tile_size - 1, sprite_y + region_mask_height - scale + 2), fill="#0000ff", outline=None)
-          # Paste into output using mask
-          source_x = bounds[0]
-          source_y = bounds[1] + row * region_mask_height
-          print("   ", "Source position", tile, (source_x, source_y, source_x + bounds[2], source_y + bounds[3]))
-          output_image.paste(source_image.crop((source_x, source_y, source_x + bounds[2] - bounds[0], source_y + bounds[3] - bounds[1])), (sprite_x, sprite_y - (bounds[3] - bounds[1]) + region_mask_height), current_mask.crop(bounds))
+          target_bounds = bounds[0] + tile * (tile_size + scale), bounds[1], bounds[2] + tile * (tile_size + scale), bounds[3]
+          print("   ", "Target position", tile, target_bounds)
+          output_image.paste(source_image.crop(bounds), target_bounds, current_mask.crop(bounds))
       output_image.save(out_path)
+    else:
+      print("Skipping", os.path.basename(out_path))
 
 if __name__ == "__main__":
   mask_regions(sys.argv[1], sys.argv[2], int(sys.argv[3]))
